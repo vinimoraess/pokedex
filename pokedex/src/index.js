92acf60d7b5e87b7c9ce4658382c4d36e0cdb5ee
vinimoraess <vinicius_jag@hotmail.com>
2022-04-25 01:08:13 -0300

    Eslint configurated

--- pokedex/src/index.js
+++ pokedex/src/index.js
@@ -1,9 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <App />
-);
+import "./index.css";
+import App from "./App.js";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(<App />);
